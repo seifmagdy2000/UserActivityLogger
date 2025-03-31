d8e482bad0936b5746b25cc32c51c72c0a84e0e1
seifmagdy2000 <seifmagdy20000@gmail.com>
2025-03-31 18:31:54 +0200

    fixed docker-compose for successful build

--- src/infrastructure/kafka/kafka.js
+++ src/infrastructure/kafka/kafka.js
@@ -1,8 +1,10 @@
 import { Kafka } from "kafkajs";
+import dotenv from "dotenv";
+dotenv.config();
 
 const kafka = new Kafka({
   clientId: "nodejs-kafka",
-  brokers: ["localhost:9092"],
+  brokers: [process.env.KAFKA_BROKER],
 });
 
 export default kafka;
